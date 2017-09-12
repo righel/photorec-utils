49d227c7793017013b447b865d485e36af2510c2
Luciano Righetti <luciano.righetti@gmail.com>
2017-09-12 09:04:16 -0300

    added recursive path walk

--- photorec_utils.py
+++ photorec_utils.py
@@ -15,13 +15,14 @@ from matplotlib import pyplot as plt
 parser = argparse.ArgumentParser(description='photorec post-recover utils.')
 
 # Required parameters
-parser.add_argument('folders', metavar='folder', type=str, nargs='+', help='Source folders.')
+parser.add_argument('dirs', metavar='dir', type=str, nargs='+', help='Source dirs.')
 
 # Optional parameters
-parser.add_argument('-r','--recursive', dest='recursive', help='Perform a recursive search inside directories.')
+parser.add_argument('-r','--recursive', dest='recursive', help='Perform a recursive search inside directories.', action='store_true')
 parser.add_argument('-v','--verbose', dest='verbose', help='Verbose output.', action='store_true')
 parser.add_argument('-ms','--min_size', dest='min_size', help='Match files with size greater or equal than this value.')
 parser.add_argument('-Ms','--max_size', dest='max_size', help='Match files with size lower or equal than this value.')
+parser.add_argument('-o','--output', dest='output', help='Output folder for copying results.')
 
 # Modes
 group = parser.add_mutually_exclusive_group(required=True)
@@ -41,6 +42,8 @@ colorama.init()
 # Banner
 print(colored("photorec post-recover utils.\n", attrs=['bold']))
 
+# Constants
+IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg", ".bmp"]
 
 def error(err):
     print(colored("[ERROR] ", "red") + err)
@@ -81,7 +84,6 @@ def blackness(filename, verbose):
     total_pixels = float(size[0] * size[1])
     zeros = float(total_pixels - non_zeros)
     zeros_ratio = zeros / total_pixels
-    print(zeros_ratio)
 
     if(verbose):
         print("blackness: {0:.2f}".format(zeros_ratio))
@@ -95,21 +97,12 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f %s%s" % (num, 'Yi', suffix)
 
-def scan_dir(folder):
-    if(not os.path.exists(folder)):
-        error(folder + ' is not a valid directory.')
+def find_files(dir, match, regex, verbose, min_size, max_size):
 
-    for root, dirs, files in os.walk(folder, topdown=False):
-        for file in files:
-            print(file)
-
-def find_files(folders, match, regex, verbose, min_size, max_size):
-
-    for folder in folders:
-        print('searching for files in ' + colored(folder, attrs=['bold']))
-        
-        if(not os.path.exists(folder)):
-            error(folder + ' is not a valid directory.')
+    print('searching for files in ' + colored(dir, attrs=['bold']))
+    
+    if(not os.path.isdir(dir)):
+        error(dir + ' is not a valid directory.')
 
     # Finds matching filenames
     if(match is None and regex is None):
@@ -119,53 +112,76 @@ def find_files(folders, match, regex, verbose, min_size, max_size):
     # Search by substring text match
     if(match is not None):
         print("searching by text match " + colored("%" + match + "%", attrs=['bold']))
-        for root, dirs, files in os.walk(folder, topdown=False):
+        for root, subdirs, files in os.walk(dir, topdown=False):
+            
             for file in files:
-
-                full_filename = folder + "/" + file
+                full_filename = os.path.join(root, file)
                 if(match is not None):
-                    try:
-                        file_info = os.stat(full_filename)
-                    except:
-                        warning("couldn't get file info for: " + file)
-                    
+
                     # Check substring match
                     if match in file:
+                    
+                        try:
+                            print(full_filename)
+                            file_info = os.stat(full_filename)
+                        except:
+                            warning("couldn't get file info for: " + file)
+                            continue
+                    
 
                         # Check size filters
                         if((min_size is None or file_info.st_size >= eval(min_size)) and (max_size is None or file_info.st_size <= eval(max_size))):
 
-                            # Print found file
-                            print('{:s} [{:s}]'.format(file, sizeof_fmt(file_info.st_size)))
 
                             # Get file extension
                             filename, file_extension = os.path.splitext(file)
 
-                            # Calculate histogram
-                            # print(full_filename)
-                            #histogram(full_filename)
-
-                            # Blackness of the image
-                            if(blackness(full_filename, verbose) > 0.65):
-                                copyfile(full_filename, folder + "/filtered/" + file)
-
                             # Get exif data if its an image
                             try: 
-                                if(lower(file_extension) in [".png", ".jpg"]):
+                                if(file_extension.lower() in IMAGE_EXTENSIONS):
                                     exif_tags = exifread.process_file(file, details=True)
                                     if(verbose):
                                         print(exif_tags)
                             except:
                                 if(verbose):
                                     warning("invalid exif tags for " + file)
+
+                            # Blackness of the image
+                            try:
+                                if(file_extension.lower() in IMAGE_EXTENSIONS):
+                                    if(blackness(full_filename, verbose) < 0.65):
+                                        continue
+                            except:
+                                error("Couldn't compute blackness for " + file)
+                                continue
+                            # If all matches, print found filename
+                            print('{:s} [{:s}]'.format(file, sizeof_fmt(file_info.st_size)))
+                            
+                            # Copy file if output dir is present
+                            if (args.output is not None):
+                                dst_dir = os.path.join(os.getcwd(), args.output)
+
+                                # Create dir if doesn't exists
+                                if(not os.path.isdir(dst_dir)):
+                                    os.mkdir(dst_dir)
+
+                                # Copy file
+                                dst_file = os.path.join(dst_dir, file)
+                                copyfile(full_filename, dst_file)
+
                 # Search by regex
                 if regex is not None:
                     pass
-        print("")
+            # Recursive walk inside paths
+            if subdirs is not None and args.recursive:
+                for subdir in subdirs:
+                    if(os.path.isdir(subdir)):
+                        find_files(subdir, match, regex, verbose, min_size, max_size)
 
     # Search by regex pattern
     if(regex is not None):
         print("searching by regex match ")
 
 if(args.find):
-    find_files(folders=args.folders, match=args.match, regex=args.regex, verbose=args.verbose, min_size=args.min_size, max_size=args.max_size)
+    for dir in args.dirs:
+        find_files(dir=dir, match=args.match, regex=args.regex, verbose=args.verbose, min_size=args.min_size, max_size=args.max_size)
